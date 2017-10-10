fc1323b63ee92b1286c1077b7626a514085b97ee
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-10 19:06:42 +0000

    added 3 stores, and updated store1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store3 = Store.find(3)
+
+@store1.name = 'Funny Store'
+@store1.save
