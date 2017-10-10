1beec0fbd03190cf8fdd2ee1a158aaa551e4ff6c
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-10 19:43:48 +0000

    finished exercise 5 to find revenue for stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,8 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+@average_store_revenue = @total_revenue / Store.all.count
+@number_stores_over_1mil = Store.where("annual_revenue > 1000000").count
+
+puts "total_revenue #{@total_revenue}, average_store_revenue #{@average_store_revenue}, number_stores_over_1mil #{@number_stores_over_1mil}"
